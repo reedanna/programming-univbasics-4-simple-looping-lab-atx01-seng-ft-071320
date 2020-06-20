950232cd0da836e9d4a380e306ceb6894230e89f
Anna Nicole Reed <reedanna@mail.com>
2020-06-20 00:42:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,11 @@ def loop_message_n_times(message, n)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+  while counter < array.length - 1
+    puts array[counter]
+    counter += 1
+  end
+end
