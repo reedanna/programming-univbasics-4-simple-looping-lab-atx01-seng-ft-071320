261d88adc8189c2a1f3d0e0877b8b9e59c813e36
Anna Nicole Reed <reedanna@mail.com>
2020-06-20 00:41:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  counter = 0
+  while counter < 5
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times(message, n)
+  counter = 0
+  while counter < n
+    puts message
+    counter += 1
+  end
+end
